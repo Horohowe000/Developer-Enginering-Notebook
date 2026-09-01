58a3a3ba6739971d8f0b2f039a8e0546a58b91fd
Horohowe000 <horohowe000@hsestudents.org>
2026-09-01 11:50:55 -0400

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+
+<html>
+    <body>
+
+ <h1>JavaScript</h1>
+
+ <p id="demo">Learn My favorite Movie.</p>
+
+ <button type="button"
+ oneclick='document.getElementById("demo").innerHTML = "My favorite 
+ movie is LOTR the Twin Towers"'>Click Me.</button>
+
+    </body>
+</html>
